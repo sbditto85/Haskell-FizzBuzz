c6c07c256975183b07a0b072122841bf7376a0f4
Gregg Hernandez <greggory.hz@gmail.com>
2013-11-19 14:12:33 -0700

    moved things out of the do

--- main.hs
+++ main.hs
@@ -1,8 +1,10 @@
-main = do
-	putStrLn $ unlines $ fizzbuzz $ take 100 [1..]
-	where fizzbuzz xl = foldr (\x acc -> (fizz x):acc ) [] xl
-		where fizz num 
-			| num `mod` 15 == 0 = "fizzbuzz"
-			| num `mod` 3 == 0 = "fizz"
-			| num `mod` 5 == 0 = "buzz"
-			| otherwise = show num
+fizzbuzz :: [String]
+fizzbuzz = foldr (\x acc -> (fizz x):acc) [] [1..]
+  where fizz num
+          | num `mod` 15 == 0 = "fizzbuzz"
+          | num `mod` 3 == 0 = "fizz"
+          | num `mod` 5 == 0 = "buzz"
+          | otherwise = show num
+
+main :: IO ()
+main = putStrLn $ unlines $ take 100 fizzbuzz
