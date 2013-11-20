3e40a6355e6e84cb24327309c5c60f6cfcf72f17
Casey Allred <sbditto85@gmail.com>
2013-11-20 10:48:49 -0700

    tidding up main, putting fizzbuzz in the global scope with a type declaration

--- main.hs
+++ main.hs
@@ -1,8 +1,9 @@
-main = do
-	putStrLn $ unlines $ take 100 fizzbuzz
-	where fizzbuzz = [fizz x | x <- [1..]]
-		where fizz num 
-			| num `mod` 15 == 0 = "fizzbuzz"
-			| num `mod` 3 == 0 = "fizz"
-			| num `mod` 5 == 0 = "buzz"
-			| otherwise = show num
+fizzbuzz :: [String]
+fizzbuzz = [fizz x | x <- [1..]]
+	where fizz num 
+		| num `mod` 15 == 0 = "fizzbuzz"
+		| num `mod` 3 == 0 = "fizz"
+		| num `mod` 5 == 0 = "buzz"
+		| otherwise = show num
+
+main = putStrLn $ unlines $ take 100 fizzbuzz
